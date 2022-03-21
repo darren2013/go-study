e299612bde5d16f5fc39721118a4c4e363145ad8
杜东方 <darren@MacBook-Pro.local>
2022-03-21 23:59:18 +0800

    httpserver demo

--- lecture16/httpServer.go
+++ lecture16/httpServer.go
@@ -1,9 +1,39 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func main() {
 
 	flag.Set("v", "4")
+
+	http.HandleFunc("/", rootHandler)
+
+	err := http.ListenAndServe(":80", nil)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func rootHandler(responseWriter http.ResponseWriter, httpRequest *http.Request) {
+	fmt.Println("root handler....")
+	user := httpRequest.URL.Query().Get("u")
+
+	if user != "" {
+		io.WriteString(responseWriter, fmt.Sprintf("hello %s \n", user))
+	} else {
+		io.WriteString(responseWriter, "hello,[stranger] \n")
+	}
+
+	io.WriteString(responseWriter, "details of the request header")
+
+	for k, v := range httpRequest.Header {
+		io.WriteString(responseWriter, fmt.Sprintf("%s=%s", k, v))
+	}
 
 }
